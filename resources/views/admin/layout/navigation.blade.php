8c2cb411ad7ef4410a72885dff2299d9b9767e00
reyfany <wahyu.re38@gmail.com>
2021-08-29 08:26:35 +0700

    navbar

--- resources/views/admin/layout/navigation.blade.php
+++ resources/views/admin/layout/navigation.blade.php
@@ -1,3 +1,10 @@
+
+<style>
+    .nav {
+        margin-left: 40px;
+    }
+  
+</style>
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
 
@@ -11,13 +18,11 @@
 
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="{{url('/administrator')}}"> <i class="menu-icon fa fa-dashboard"></i>Beranda </a>
-                    <a href="{{url('/administrator/galeri')}}"> <i class="menu-icon fa fa-picture-o"></i>Galeri </a>
-                    <a href="{{url('/administrator/berita')}}"> <i class="menu-icon fa fa-file-text-o"></i>Berita </a>
-                    <a href="{{url('/administrator/guru')}}"> <i class="menu-icon fa fa-file-text-o"></i>Guru </a>
-                    <a href="{{url('/administrator/siswa')}}"> <i class="menu-icon fa fa-file-text-o"></i>Siswa </a>
-                </li>
+                <li><a href="{{url('/administrator')}}"><i class="menu-icon fa fa-dashboard"></i>Beranda</a></li>
+                <li><a href="{{url('/administrator/galeri')}}"> <i class="menu-icon fa fa-picture-o"></i>Galeri </a></li>
+                <li><a href="{{url('/administrator/berita')}}"> <i class="menu-icon fa fa-book"></i>Berita </a></li>
+                <li><a href="{{url('/administrator/guru')}}"> <i class="menu-icon fa fa-male"></i>Guru </a></li>   
+                <li><a href="{{url('/administrator/siswa')}}"> <i class="menu-icon fa fa-user"></i>Siswa </a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </nav>
